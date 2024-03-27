9538c7ab6254bf2638a0e7d485948e9a99e07e17
cps <3170647600@qq.com>
2024-03-27 16:12:05 +0800

    2024-3-25-搭建MQ消息组件Kafka服务环境

---
+++ lottery-application/src/main/java/com/cps/lottery/application/mq/KafkaConsumer.java
@@ -0,0 +1,36 @@
+package com.cps.lottery.application.mq;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @author cps
+ * @description: 消息消费者
+ * @date 2024/3/27 15:46
+ * @OtherDescription: Other things
+ */
+
+@Component
+public class KafkaConsumer {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP)
+    public void topicTest(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
+        Optional<?> message = Optional.ofNullable(record.value());
+        if(message.isPresent()){
+            Object msg = message.get();
+            logger.info("topic_test消费了： Topic: " + topic + ",Message: " + msg);
+            ack.acknowledge();
+        }
+
+    }
+}
---
+++ lottery-interfaces/src/test/java/com/cps/lottery/test/application/KafkaProducerTest.java
@@ -0,0 +1,37 @@
+package com.cps.lottery.test.application;
+
+import com.cps.lottery.application.mq.KafkaProducer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author cps
+ * @description: TODO
+ * @date 2024/3/27 16:04
+ * @OtherDescription: Other things
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaProducerTest.class);
+
+    @Resource
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void test_send() throws InterruptedException {
+        // 循环发送消息
+        while (true) {
+            kafkaProducer.send("你好，我是Lottery 001");
+            Thread.sleep(3500);
+        }
+    }
+}
